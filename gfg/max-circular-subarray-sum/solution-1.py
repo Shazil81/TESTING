07e09e90529f762d4f65785f0d6ea75977f2e7e3
Shazil81 <ahmadshahab890@gmail.com>
2026-09-17 18:44:32 +0530

    Add Max Circular Subarray Sum (gfg)

---
+++ gfg/max-circular-subarray-sum/solution-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxCircularSum(self, arr):
+        # kadane'n algo (logic yhi hai ki total sum se min sum ko minus kr do tb hi wo circular wala max dega)
+        # optimal hai
+        total_sum = 0
+        max_sub = arr[0]
+        min_sub = arr[0]
+        curr_max = 0
+        curr_min = 0
+
+        for num in arr:
+            total_sum += num
+            curr_max = max(num, curr_max+num)
+            max_sub = max(max_sub, curr_max)
+
+            curr_min = min(num, curr_min+num)
+            min_sub = min(min_sub, curr_min)
+
+        if max_sub < 0:
+            return max_sub
+
+        circular_max = total_sum - min_sub
+
+        return max(max_sub, circular_max)
+        
